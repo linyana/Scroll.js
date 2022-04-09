72ca16d5c2c7cdd85d4f212fd97c3d46bd10b121
linyana <1439713475@qq.com>
2022-04-09 19:36:29 +0800

    update

--- Scroll.js
+++ Scroll.js
@@ -1,6 +1,9 @@
 // Prevents the browser from remembering the scroll position.
 history.scrollRestoration = 'manual';
 
+// Hide the browse scroll bar
+document.documentElement.style.overflowY = 'hidden'; 
+
 class Lin {
     /*
     * @function initialize 
@@ -8,13 +11,25 @@ class Lin {
     * @return void
     */
     constructor(){
-        
+        this.linNum = 0
     }
 
     linGetContainer(element){
         const HTMLelement = document.getElementsByTagName(element)
         Array.from(HTMLelement).forEach((element) => {
-            element.style.position = 'relative'; 
+            element.style.position = 'relative';
+            element.style.display = 'inline-block';
+            element.style.height = window.screen.availWidth + 'px';
+            element.style.width = 100 + '%';
+
+            /*
+            * @function control the way with mouse scrolls
+            */
+            element.addEventListener('wheel',(event)=>{
+                event.preventDefault();
+                this.linNum -= event.deltaY;
+                this.linGetBox('lin-box')
+            })
         })
     }
 
@@ -22,13 +37,15 @@ class Lin {
         const HTMLelement = document.getElementsByTagName(element)
         Array.from(HTMLelement).forEach((element) => {
             element.style.position = 'absolute';
+            element.style.transition = 'top' + ' ' + '0.9s' + ' ' + 'ease-out' 
             if (element.getAttribute('l')) {
-                element.style.left = element.getAttribute('l') + 'px';
+                element.style.left = element.getAttribute('l') +'px';
             } else if (element.getAttribute('r')) {
                 element.style.right = element.getAttribute('r') + 'px';
             }
             if (element.getAttribute('t')) {
-                element.style.top = element.getAttribute('t') + 'px';
+                element.style.top = Number(element.getAttribute('t')) + this.linNum + 'px';
+                console.log(element.getAttribute('t') + this.linNum + 'px');
             } else if (element.getAttribute('b')) {
                 element.style.bottom = element.getAttribute('b') + 'px';
             }
