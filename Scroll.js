599e63d9d310ddebdb3b2bf6416e6801834b2bbe
linyana <1439713475@qq.com>
2022-04-21 18:30:35 +0800

    update

--- Scroll.js
+++ Scroll.js
@@ -2,7 +2,7 @@
 history.scrollRestoration = 'manual';
 
 // Hide the browse scroll bar
-document.documentElement.style.overflowY = 'hidden'; 
+document.documentElement.style.overflowY = 'hidden';
 
 class Lin {
     /*
@@ -10,7 +10,7 @@ class Lin {
     * @param 
     * @return void
     */
-    constructor(){
+    constructor() {
         this.linNum = 0;
     }
 
@@ -19,8 +19,8 @@ class Lin {
     * @param element{string} tag name ('lin-container')
     * @return void
     */
-    linGetContainer(element){
-        const HTMLelement = document.getElementsByTagName(element)
+    linGetContainer(element) {
+        const HTMLelement = document.getElementsByTagName(element);
         Array.from(HTMLelement).forEach((element) => {
             element.style.position = 'relative';
             element.style.display = 'inline-block';
@@ -30,13 +30,13 @@ class Lin {
             /*
             * @function control the way with mouse scrolls
             */
-            element.addEventListener('wheel',(event)=>{
+            element.addEventListener('wheel', (event) => {
                 event.preventDefault();
                 this.linNum -= event.deltaY;
-                if(this.linNum >= 0){
+                if (this.linNum >= 0) {
                     this.linNum = 0;
                 }
-                if (this.linNum <= -window.screen.availHeight) { 
+                if (this.linNum <= -window.screen.availHeight) {
                     this.linNum = -window.screen.availHeight;
                 }
                 this.linGetBox('lin-box');
@@ -49,40 +49,46 @@ class Lin {
     * @param element{string} tag name ('lin-box')
     * @return void
     */
-    linGetBox(element) { 
-        const HTMLelement = document.getElementsByTagName(element)
+    linGetBox(element) {
+        const HTMLelement = document.getElementsByTagName(element);
         Array.from(HTMLelement).forEach((element) => {
             element.style.position = 'absolute';
             element.style.transition = 'top' + ' ' + '0.9s' + ' ' + 'ease-out';
             if (element.getAttribute('l')) {
-                element.style.left = element.getAttribute('l') +'px';
+                element.style.left = element.getAttribute('l') + 'px';
             } else if (element.getAttribute('r')) {
                 element.style.right = element.getAttribute('r') + 'px';
             }
             if (element.getAttribute('t')) {
                 element.style.top = Number(element.getAttribute('t')) + this.linNum + 'px';
-                console.log(element.getAttribute('t') + this.linNum + 'px');
             } else if (element.getAttribute('b')) {
                 element.style.bottom = element.getAttribute('b') + 'px';
             }
-            if (element.getAttribute('w')) { 
+            if (element.getAttribute('w')) {
                 element.style.width = element.getAttribute('w') + 'px';
             }
-            if (element.getAttribute('h')) { 
+            if (element.getAttribute('h')) {
                 element.style.height = element.getAttribute('h') + 'px';
             }
         })
+    }
+
+    linGetScrollBar() {
+        const scrollBar = document.getElementsByTagName('lin-scroll-bar');
     }
 }
 
 let lin = new Lin()
 
-window.addEventListener('load',function(){
-    if(document.getElementsByTagName('lin-container')){
+window.addEventListener('load', function () {
+    if (document.getElementsByTagName('lin-container')) {
         lin.linGetContainer('lin-container');
     }
-    if(document.getElementsByTagName('lin-box')){
+    if (document.getElementsByTagName('lin-box')) {
         lin.linGetBox('lin-box');
+    }
+    if (document.getElementsByTagName('lin-scroll-bar')) {
+        lin.linGetScrollBar()
     }
 })
 
