306a12d336defefb4760f825cb0aa81e947b4174
lin <1439713475@qq.com>
2022-10-16 23:31:02 +0800

    修复了当根元素是用margin居中时会出现的错误

--- Scroll.js
+++ Scroll.js
@@ -51,7 +51,19 @@ class Lin {
                 if (document.getElementsByTagName('lin-scroll-bar')) {
                     this.linScrollBar();
                 }
+                this.linSetPage();
             })
+        });
+    }
+
+    linSetPage() {
+        const HTMLelement = document.getElementsByTagName('lin-page');
+        Array.from(HTMLelement).forEach((element) => {
+            element.style.position = 'absolute';
+            element.style.top = -this.linNum + 'px';
+            element.style.width = '100%';
+            element.style.height = '100%';
+            element.style.transition = 'all 0.8s ease-out';
         });
     }
 
@@ -61,23 +73,25 @@ class Lin {
         const HTMLelement = document.getElementsByTagName('lin-box');
         // 遍历linBox并给linBox设置一些基本属性
         Array.from(HTMLelement).forEach((element) => {
-            for (let data of this.linData.linBox) {
-                if (element.getAttribute('lin-name') === data.name) {
-                    element.style.position = 'absolute';
-                    if (data.direction) {
-                        element.style.transition = data.direction + ' 1s ease-out';
-                    } else {
-                        element.style.transition = 'top 1s ease-out'
+            if (this.linData.linBox) {
+                for (let data of this.linData.linBox) {
+                    if (element.getAttribute('lin-name') === data.name) {
+                        element.style.position = 'absolute';
+                        if (data.direction) {
+                            element.style.transition = data.direction + ' 1s ease-out';
+                        } else {
+                            element.style.transition = 'top 1s ease-out'
+                        }
+                        element.style.left = data.left + 'px' || 'auto';
+                        element.style.top = data.top + 'px' || 'auto';
+                        element.style.right = data.right + 'px' || 'auto';
+                        element.style.bottom = data.bottom + 'px' || 'auto';
                     }
-                    element.style.left = data.left + 'px' || 'auto';
-                    element.style.top = data.top + 'px' || 'auto';
-                    element.style.right = data.right + 'px' || 'auto';
-                    element.style.bottom = data.bottom + 'px' || 'auto';
-                }
-                else if (element.getAttribute('lin-name') === null) {
-                    element.style.position = 'absolute';
-                    element.style.transition = 'all 1s ease-out';
-                    element.style.top = -this.linNum + 'px';
+                    else if (element.getAttribute('lin-name') === null) {
+                        element.style.position = 'absolute';
+                        element.style.transition = 'all 1s ease-out';
+                        element.style.top = -this.linNum + 'px';
+                    }
                 }
             }
         })
@@ -87,35 +101,37 @@ class Lin {
     linScrollBox() {
         const HTMLelement = document.getElementsByTagName('lin-box');
         Array.from(HTMLelement).forEach((element) => {
-            for (let data of this.linData.linBox) {
-                if (element.getAttribute('lin-name') === data.name) {
-                    let direction = data.direction || 'top';
-                    // 如果start和end同时不为空
-                    if (data.start && data.end) {
-                        // 判断开始和结尾
-                        if (data.start <= data.end) {
-                            if (this.linNum <= data.start) {
-                                element.style[direction] = data.from + 'px';
-                            } else if (this.linNum >= data.end) {
-                                element.style[direction] = data.to + 'px';
-                            }
-                            if (this.linNum >= data.start && this.linNum <= data.end) {
-                                // 如果设置了起点和终点
-                                if (data.from && data.to) {
-                                    element.style[direction] = (((this.linNum - data.start) / (data.end - data.start)) * (data.to - data.from)) + data.from + 'px';
-                                } else {
-                                    element.style[direction] = this.linNum + 'px';
+            if (this.linData.linBox) {
+                for (let data of this.linData.linBox) {
+                    if (element.getAttribute('lin-name') === data.name) {
+                        let direction = data.direction || 'top';
+                        // 如果start和end同时不为空
+                        if (data.start && data.end) {
+                            // 判断开始和结尾
+                            if (data.start <= data.end) {
+                                if (this.linNum <= data.start) {
+                                    element.style[direction] = data.from + 'px';
+                                } else if (this.linNum >= data.end) {
+                                    element.style[direction] = data.to + 'px';
+                                }
+                                if (this.linNum >= data.start && this.linNum <= data.end) {
+                                    // 如果设置了起点和终点
+                                    if (data.from && data.to) {
+                                        element.style[direction] = (((this.linNum - data.start) / (data.end - data.start)) * (data.to - data.from)) + data.from + 'px';
+                                    } else {
+                                        element.style[direction] = this.linNum + 'px';
+                                    }
                                 }
                             }
-                        }
-                        // 判断开始和结尾
-                        else if (data.start > data.end) {
-                            console.error('请勿把end设置成大于start的值')
+                            // 判断开始和结尾
+                            else if (data.start > data.end) {
+                                console.error('请勿把end设置成大于start的值')
+                            }
                         }
                     }
-                }
-                else if (element.getAttribute('lin-name') === null) {
-                    element.style.top = -this.linNum + 'px';
+                    else if (element.getAttribute('lin-name') === null) {
+                        element.style.top = -this.linNum + 'px';
+                    }
                 }
             }
         });
@@ -143,6 +159,15 @@ class Lin {
             e.style.left = '1px';
             e.style.borderRadius = '4px';
             e.style.transition = 'all 0.8s ease-out'
+            if (this.linData.linScrollBar) {
+                let data = this.linData.linScrollBar;
+                e.style.backgroundColor = data.bgColor || '#3bbbbb';
+                e.style.height = data.height + 'px' || '300px';
+                if (data.fatherShow) {
+                    element.style.border = 'none';
+                    element.style.backgroundColor = 'transparent';
+                }
+            }
         })
     }
 
